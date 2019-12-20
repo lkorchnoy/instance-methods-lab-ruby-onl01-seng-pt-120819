0fea5ba854c4fb64b89098c4ef01f8e90947fe23
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-20 19:58:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,21 @@
 class Dog
+  def bark
+  puts "Woof!"
 end
+
+  def sit
+    puts ""
+  
+  
+end
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
+ 
+snoopy = Dog.new
+snoopy.bark #> "Woof!"
